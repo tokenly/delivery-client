fc8050ebfce5cf1d057973f9c99c50da4665da31
cryptonaut <nrathman@ironcladtech.ca>
2016-10-19 02:13:00 -0700

    added matching methods for delivery api endpoints

--- src/Tokenly/DeliveryClient/Client.php
+++ src/Tokenly/DeliveryClient/Client.php
@@ -21,7 +21,8 @@ class Client
         $this->api_token      = $api_token;
         $this->api_secret_key = $api_secret_key;
     }
-
+    
+    /** Source Address Methods **/
 
     public function newSourceAddress($label, $type, $webhook = null, $join_callback = null, $auto_fulfill = true)
     {
@@ -64,6 +65,144 @@ class Client
         $data = array('sweep_address' => $sweep_address);
         return $this->newAPIRequest('DELETE', '/source/'.$uuid, $data);
     }
+    
+    
+    /** Token Delivery Methods **/
+    
+    public function newDelivery($source, $destination, $token, $quantity, $delivery_date = null, $extra_opts = array())
+    {
+        $data = array();
+        $data['source'] = $source;
+        $data['destination'] = $destination;
+        $data['token'] = $token;
+        $data['quantity'] = $quantity;
+        $data['delivery_date'] = $delivery_date;
+        if(isset($extra_opts['pseudo']) AND $extra_opts['pseudo']){
+            $data['pseudo'] = true;
+        }
+        if(isset($extra_opts['hold_promise']) AND $extra_opts['hold_promise']){
+            $data['hold_promise'] = true;
+        }
+        if(isset($extra_opts['ref_data'])){
+            $data['ref_data'] = $extra_opts['ref_data'];
+        }
+        $delivery = false;
+        try{
+            $delivery = $this->newAPIRequest('POST', '/delivery', $data);
+        }
+        catch(DeliveryException $e){
+            throw new Exception('Error creating delivery: '.$e->getMessage());
+        }
+        if(!isset($delivery['result'])){
+            throw new Exception('Unknown error creating token delivery');
+        }
+        return $delivery['result'];
+    }
+    
+    public function getDelivery($uuid)
+    {
+        $delivery = false;
+        try{
+            $delivery = $this->newAPIRequest('GET', '/delivery/'.$uuid);
+        }
+        catch(DeliveryException $e){
+            throw new Exception('Error getting details: '.$e->getMessage());
+        }
+        if(!isset($delivery['result'])){
+            throw new Exception('Unknown error getting delivery details');
+        }
+        return $delivery['result'];
+    }
+    
+    public function getDeliveryList($filters = array())
+    {
+        $use_filters = array();
+        $valid_filters = array('source', 'token', 'destination', 'show_complete');
+        foreach($valid_filters as $f){
+            if(isset($filters[$f])){
+                $use_filters[$f] = $filters[$f];
+            }
+        }
+        $output = false;
+        try{
+            $output = $this->newAPIRequest('GET', '/delivery', $use_filters);
+        }
+        catch(DeliveryException $e){
+            throw new Exception($e->getMessage());
+        }
+        return $output;
+    }
+    
+    public function updateDelivery($uuid, $data)
+    {
+        $use_data = array();
+        $valid_fields = array('quantity', 'delivery_date', 'ref_data', 'ready');
+        foreach($valid_fields as $f){
+            if(isset($data[$f])){
+                $use_data[$f] = $data[$f];
+            }
+        }
+        $update = false;
+        try{
+            $update = $this->newAPIRequest('PATCH', '/delivery/'.$uuid, $use_data);
+        }
+        catch(DeliveryException $e){
+            throw new Exception('Error updating delivery: '.$e->getMessage());
+        }
+        if(!$update){
+            throw new Exception('Unknown error updating delivery');
+        }
+        return $output; 
+    }
+    
+    public function markReady($uuid)
+    {
+        $use_data = array();
+        $use_data['ready'] = true;
+        $update = false;
+        try{
+            $update = $this->newAPIRequest('PATCH', '/delivery/'.$uuid, $use_data);
+        }
+        catch(DeliveryException $e){
+            throw new Exception('Error marking delivery as ready to fulfill: '.$e->getMessage());
+        }
+        if(!$update){
+            throw new Exception('Unknown error marking delivery ready to fulfill');
+        }
+        return $output; 
+    }
+    
+    public function markUnready($uuid)
+    {
+        $use_data = array();
+        $use_data['ready'] = false;
+        $update = false;
+        try{
+            $update = $this->newAPIRequest('PATCH', '/delivery/'.$uuid, $use_data);
+        }
+        catch(DeliveryException $e){
+            throw new Exception('Error marking delivery as no longer ready to fulfill: '.$e->getMessage());
+        }
+        if(!$update){
+            throw new Exception('Unknown error marking delivery no longer ready to fulfill');
+        }
+        return $output; 
+    }
+    
+    public function cancelDelivery($uuid)
+    {
+        $delete = false;
+        try{
+            $delete = $this->newAPIRequest('DELETE', '/delivery/'.$uuid);
+        }
+        catch(DeliveryException $e){
+            throw new Exception('Error canceling delivery: '.$e->getMessage());
+        }
+        if(!$delete){
+            throw new Exception('Unknown error canceling delivery');
+        }
+        return $output; 
+    }
 
 
     ////////////////////////////////////////////////////////////////////////
@@ -100,16 +239,13 @@ class Client
                     // could not parse json
                     $json = null;
                 }
-                if ($json and isset($json['message'])) {
-                    // throw an DeliveryException with the errorName
-                    if (isset($json['errorName'])) {
-                        $delivery_exception = new DeliveryException($json['message'], $code);
-                        $delivery_exception->setErrorName($json['errorName']);
-                        throw $delivery_exception;
-                    }
-
-                    // generic exception
-                    throw new Exception($json['message'], $code);
+                if ($json and isset($json['error'])) {
+                    $delivery_exception = new DeliveryException($json['error'], $code);
+                    throw $delivery_exception;
+                }
+                elseif($json AND isset($json['message'])){
+                    $delivery_exception = new DeliveryException($json['message'], $code);
+                    throw $delivery_exception;
                 }
             }
 
